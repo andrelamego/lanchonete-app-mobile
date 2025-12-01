25722e05ead4aad2fd2fe4e9587aa3e6284c81ec
André Lamego <andreolamego@gmail.com>
2025-12-01 04:10:58 -0300

    feat: Refatora a tela principal e a tela de funcionário
    Refatora a interface do usuário da `MainActivity` para um design mais moderno, utilizando `ScrollView`, `LinearLayout` e `ConstraintLayout` para organizar os botões de navegação. Adiciona ícones e novos recursos de cores.
    
    Na `FuncionarioActivity`, a interface é redesenhada com um cabeçalho, campos de entrada de texto estilizados e um botão de salvar. Adiciona a funcionalidade de voltar para a tela anterior.
    
    As activities agora são devidamente declaradas no `AndroidManifest.xml` e novas strings e cores são adicionadas aos recursos do projeto.

--- app/src/main/java/com/fatec/lanchonetemobile/adapters/ui/MainActivity.java
+++ app/src/main/java/com/fatec/lanchonetemobile/adapters/ui/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,73 +15,66 @@ import com.fatec.lanchonetemobile.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnCadProduto;
-    private Button btnCadFuncionario;
-    private Button btnCadCliente;
-    private Button btnCadPedido;
-    private Button btnCadFornecedor;
-    private Button btnCadCargo;
-    private Button btnCadCategoria;
-    private Button btnGerenciarPedido;
+    private Button btnProdutos;
+    private Button btnFuncionarios;
+    private Button btnClientes;
+    private Button btnPedidos;
+    private ConstraintLayout btnFornecedores;
+    private Button btnCargos;
+    private Button btnCategorias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainActivity), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        btnCadProduto = findViewById(R.id.btnCadProduto);
-        btnCadFuncionario = findViewById(R.id.btnCadFuncionario);
-        btnCadCliente = findViewById(R.id.btnCadCliente);
-        btnCadPedido = findViewById(R.id.btnCadPedido);
-        btnCadFornecedor = findViewById(R.id.btnCadFornecedor);
-        btnCadCargo = findViewById(R.id.btnCadCargo);
-        btnCadCategoria = findViewById(R.id.btnCadCategoria);
-        btnGerenciarPedido = findViewById(R.id.btnGerenciarPedido);
-
-        btnCadProduto.setOnClickListener(e -> {
-            Intent intent = new Intent(this, ProdutoActivity.class);
-            startActivity(intent);
-        });
-
-        btnCadFuncionario.setOnClickListener(e -> {
-            Intent intent = new Intent(this, FuncionarioActivity.class);
-            startActivity(intent);
-        });
-
-        btnCadCliente.setOnClickListener(e -> {
-            Intent intent = new Intent(this, ClienteActivity.class);
-            startActivity(intent);
-        });
-
-        btnCadPedido.setOnClickListener(e -> {
-            Intent intent = new Intent(this, PedidoActivity.class);
-            startActivity(intent);
-        });
-
-        btnCadFornecedor.setOnClickListener(e -> {
+//        btnProdutos = findViewById(R.id.btnProdutos);
+//        btnFuncionarios = findViewById(R.id.btnFuncionarios);
+//        btnClientes = findViewById(R.id.btnClientes);
+//        btnPedidos = findViewById(R.id.btnPedidos);
+        btnFornecedores = findViewById(R.id.btnFornecedores);
+//        btnCargos = findViewById(R.id.btnCargos);
+//        btnCategorias = findViewById(R.id.btnCategorias);
+//
+//        btnProdutos.setOnClickListener(e -> {
+//            Intent intent = new Intent(this, ProdutoActivity.class);
+//            startActivity(intent);
+//        });
+//
+//        btnFuncionarios.setOnClickListener(e -> {
+//            Intent intent = new Intent(this, FuncionarioActivity.class);
+//            startActivity(intent);
+//        });
+//
+//        btnClientes.setOnClickListener(e -> {
+//            Intent intent = new Intent(this, ClienteActivity.class);
+//            startActivity(intent);
+//        });
+//
+//        btnPedidos.setOnClickListener(e -> {
+//            Intent intent = new Intent(this, PedidoActivity.class);
+//            startActivity(intent);
+//        });
+//
+        btnFornecedores.setOnClickListener(e -> {
             Intent intent = new Intent(this, FornecedorActivity.class);
             startActivity(intent);
         });
-
-        btnCadCargo.setOnClickListener(e -> {
-            Intent intent = new Intent(this, CargoActivity.class);
-            startActivity(intent);
-        });
-
-        btnCadCategoria.setOnClickListener(e -> {
-            Intent intent = new Intent(this, CategoriaActivity.class);
-            startActivity(intent);
-        });
-
-        btnGerenciarPedido.setOnClickListener(e -> {
-            Intent intent = new Intent(this, GerenciarPedidoActivity.class);
-            startActivity(intent);
-        });
+//
+//        btnCargos.setOnClickListener(e -> {
+//            Intent intent = new Intent(this, CargoActivity.class);
+//            startActivity(intent);
+//        });
+//
+//        btnCategorias.setOnClickListener(e -> {
+//            Intent intent = new Intent(this, CategoriaActivity.class);
+//            startActivity(intent);
+//        });
     }
 }
